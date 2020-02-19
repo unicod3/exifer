352cdb20804756114e15a66b6d77e1217ef13846
unicod3 <sinanulker386@gmail.com>
2020-02-20 01:09:30 +0300

    refactor for streaming

--- main.go
+++ main.go
@@ -1,50 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"os/exec"
-	"sync"
 )
 
-type Taginfo struct {
-	XMLName xml.Name `xml:"taginfo"`
-	Text    string   `xml:",chardata"`
-	Table   []struct {
+type TagXML struct {
+	Text     string `xml:",chardata"`
+	ID       string `xml:"id,attr"`
+	Name     string `xml:"name,attr"`
+	Type     string `xml:"type,attr"`
+	Writable string `xml:"writable,attr"`
+	G2       string `xml:"g2,attr"`
+	Desc     []struct {
 		Text string `xml:",chardata"`
-		Name string `xml:"name,attr"`
-		G0   string `xml:"g0,attr"`
-		G1   string `xml:"g1,attr"`
-		G2   string `xml:"g2,attr"`
-		Desc struct {
-			Text string `xml:",chardata"`
-			Lang string `xml:"lang,attr"`
-		} `xml:"desc"`
-		Tag []struct {
-			Text     string `xml:",chardata"`
-			ID       string `xml:"id,attr"`
-			Name     string `xml:"name,attr"`
-			Type     string `xml:"type,attr"`
-			Writable string `xml:"writable,attr"`
-			G2       string `xml:"g2,attr"`
-			Desc     []struct {
-				Text string `xml:",chardata"`
-				Lang string `xml:"lang,attr"`
-			} `xml:"desc"`
-		} `xml:"tag"`
-	} `xml:"table"`
+		Lang string `xml:"lang,attr"`
+	} `xml:"desc"`
+}
+
+type TableXML struct {
+	Text string `xml:",chardata"`
+	Name string `xml:"name,attr"`
+	G0   string `xml:"g0,attr"`
+	G1   string `xml:"g1,attr"`
+	G2   string `xml:"g2,attr"`
+	Desc struct {
+		Text string `xml:",chardata"`
+		Lang string `xml:"lang,attr"`
+	} `xml:"desc"`
+	Tag []TagXML `xml:"tag"`
 }
 
 func main() {
-	fmt.Println("Please visit:  http://localhost:3333/tags")
+	log.Println("Please visit:  http://localhost:3333/tags")
 	// handle the endpoint
-	http.HandleFunc("/tags", handleTagsV2)
+	http.HandleFunc("/tags", handleTags)
 	if err := http.ListenAndServe(":3333", nil); err != nil {
 		panic(err)
 	}
@@ -61,120 +56,130 @@ type Response struct {
 	Tags []Tag `json:"tags"`
 }
 
-func handeStream(w io.Writer, r io.Reader) ([]byte, error) {
-	var out []byte
-	buf := make([]byte, 1024, 1024)
-	for {
-		n, err := r.Read(buf[:])
-		if n > 0 {
-			d := buf[:n]
-			out = append(out, d...)
-			_, err := w.Write(d)
+var (
+	xmlData  chan string
+	jsonData chan Response
+	quit     chan int
+)
+
+func handleXMLStream(ctx context.Context) {
+	select {
+	case data := <-xmlData:
+
+		// response struct
+		var resp Response
+
+		// initiate buffer and Decoder for streaming
+		b := bytes.NewBufferString(data)
+		d := xml.NewDecoder(b)
+		for {
+
+			// XML token in the stream
+			t, err := d.Token()
+
 			if err != nil {
-				return out, err
+				break
 			}
-		}
-		if err != nil {
-			// Read returns io.EOF at the end of file, which is not an error for us
-			if err == io.EOF {
-				err = nil
-			}
-			return out, err
-		}
-	}
-}
 
-func handleXML(xmlVal []byte, outch chan<- []byte) {
-	xmlStr := string(xmlVal)
-	var taginfo Taginfo
-	data := []byte(xmlStr)
-	err := xml.Unmarshal(data, &taginfo)
-	if err != nil {
-		fmt.Println(err)
-	}
+			// get an instance
 
-	var resp Response
-	// generate result with the xml
-	for _, table := range taginfo.Table {
-		for _, tag := range table.Tag {
-			var t Tag
-			t.Writable = tag.Writable
-			t.Path = table.Name + ":" + tag.Name
-			t.Group = table.Name
-			t.Type = tag.Type
-			for _, desc := range tag.Desc {
-				d := map[string]string{
-					desc.Lang: desc.Text,
+			switch et := t.(type) {
+
+			case xml.StartElement:
+				// get the desired element when it starts
+				if et.Name.Local == "table" {
+					tableXml := &TableXML{}
+
+					// Decode it
+					if err := d.DecodeElement(&tableXml, &et); err != nil {
+						log.Fatal(err)
+					}
+
+					// Populate the data
+					for _, tagXml := range tableXml.Tag {
+						var tag Tag
+						tag.Path = tagXml.Name
+						tag.Writable = tagXml.Writable
+						tag.Path = tableXml.Name + ":" + tagXml.Name
+						tag.Group = tableXml.Name
+						tag.Type = tagXml.Type
+
+						for _, val := range tagXml.Desc {
+							d := map[string]string{
+								val.Lang: val.Text,
+							}
+							tag.Description = append(tag.Description, d)
+						}
+						resp.Tags = append(resp.Tags, tag)
+					}
+				} else if et.Name.Local == "taginfo" {
+					log.Println("XML Decode Stream begins")
 				}
 
-				t.Description = append(t.Description, d)
-			}
-			resp.Tags = append(resp.Tags, t)
-		}
-	}
+			case xml.EndElement:
 
-	b, err := json.Marshal(resp)
-	if err != nil {
-		fmt.Println(err)
+				if et.Name.Local != "taginfo" {
+					continue
+				}
+				if et.Name.Local != "table" {
+					continue
+				}
+			}
+		}
+		// push some bits
+		jsonData <- resp
+	case <-ctx.Done():
+		quit <- 1
 	}
-	outch <- b
 }
 
-/*
- * HTTP Endpoints
- */
-
-/*
- * handle Tags V2 handles the cmd output stream and
- */
-func handleTagsV2(w http.ResponseWriter, r *http.Request) {
+func handleTags(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	w.Header().Set("Content-Type", "application/json")
+	// initiate the chanels
+	xmlData = make(chan string)
+	jsonData = make(chan Response)
+	quit = make(chan int)
 
-	var wg sync.WaitGroup
-	res := make(chan []byte)
+	// handle the xml
+	go handleXMLStream(ctx)
+
 	cmd := exec.Command("exiftool", "-listx")
-
-	var stdout []byte
-	var errStdout, errStderr error
-	stdoutIn, _ := cmd.StdoutPipe()
-	err := cmd.Start()
-	if err != nil {
-		log.Fatalf("cmd.Start() failed with '%s'\n", err)
-	}
-
-	// use wait group to handle stream data
-	wg.Add(1)
-	go func(cctx context.Context) {
-		//defer wg.Done()
-		//d := make(chan []byte)
-		stdout, errStdout = handeStream(os.Stdout, stdoutIn)
-		wg.Done()
-		select {
-		case <-cctx.Done():
-			if err := cmd.Process.Kill(); err != nil {
-				log.Fatal("failed to kill process: ", err)
-			}
-			log.Println("Process Killed")
-			return
-		}
-	}(ctx)
-	wg.Wait()
-
-	err = cmd.Wait()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	if errStdout != nil || errStderr != nil {
-		log.Fatal("failed to capture stdout or stderr\n")
-	}
-
-	// fire a goroutine
-	go handleXML(stdout, res)
-
-	// wait for the result and pass the stream to browser
-	_, err = w.Write(<-res)
+	_, err := cmd.StdinPipe()
 	if err != nil {
 		w.Write([]byte("Something went wrong"))
+		return
+	}
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	xmlData <- out.String()
+
+	select {
+	case d := <-jsonData:
+		// Wait for decoded data
+		log.Println("JSON Encode stream begins")
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			panic("expected http.ResponseWriter to be an http.Flusher")
+		}
+
+		// Encode data and push some bits into screen
+		w.Header().Set("Content-Type", "application/json")
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(&d); err != nil {
+			log.Println(err)
+		}
+		flusher.Flush()
+		log.Println("Operation Completed")
+	case <-ctx.Done():
+		cmd.Process.Kill()
+		log.Println("Command Killed!")
+	case <-quit:
+		cmd.Process.Kill()
+		log.Println("Command Killed")
 	}
 }
