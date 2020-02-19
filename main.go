7d64cb05237568d2fb7d794a03c8f34a8370fdd0
unicod3 <sinanulker386@gmail.com>
2020-02-19 20:01:35 +0300

    version2

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"sync"
 )
 
 type Taginfo struct {
@@ -41,7 +44,7 @@ type Taginfo struct {
 func main() {
 	fmt.Println("Please visit:  http://localhost:3333/tags")
 	// handle the endpoint
-	http.HandleFunc("/tags", handleTags)
+	http.HandleFunc("/tags", handleTagsV2)
 	if err := http.ListenAndServe(":3333", nil); err != nil {
 		panic(err)
 	}
@@ -58,12 +61,36 @@ type Response struct {
 	Tags []Tag `json:"tags"`
 }
 
-func handleXML(xmlStr string) []byte {
+func handeStream(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			_, err := w.Write(d)
+			if err != nil {
+				return out, err
+			}
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
+	}
+}
+
+func handleXML(xmlVal []byte, outch chan<- []byte) {
+	xmlStr := string(xmlVal)
 	var taginfo Taginfo
 	data := []byte(xmlStr)
 	err := xml.Unmarshal(data, &taginfo)
 	if err != nil {
-		return []byte("")
+		fmt.Println(err)
 	}
 
 	var resp Response
@@ -83,40 +110,71 @@ func handleXML(xmlStr string) []byte {
 				t.Description = append(t.Description, d)
 			}
 			resp.Tags = append(resp.Tags, t)
-			//fmt.Printf("%v", tag)
 		}
 	}
 
-	//fmt.Printf("%#v\n\n", taginfo)
 	b, err := json.Marshal(resp)
 	if err != nil {
 		fmt.Println(err)
-		return []byte("")
 	}
-	return b
+	outch <- b
 }
 
-func handleTags(w http.ResponseWriter, r *http.Request) {
+/*
+ * HTTP Endpoints
+ */
+
+/*
+ * handle Tags V2 handles the cmd output stream and
+ */
+func handleTagsV2(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	w.Header().Set("Content-Type", "application/json")
+
+	var wg sync.WaitGroup
+	res := make(chan []byte)
 	cmd := exec.Command("exiftool", "-listx")
 
-	_, err := cmd.StdinPipe()
+	var stdout []byte
+	var errStdout, errStderr error
+	stdoutIn, _ := cmd.StdoutPipe()
+	err := cmd.Start()
 	if err != nil {
-		w.Write([]byte("Something went wrong"))
-		return
+		log.Fatalf("cmd.Start() failed with '%s'\n", err)
 	}
 
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
+	// use wait group to handle stream data
+	wg.Add(1)
+	go func(cctx context.Context) {
+		//defer wg.Done()
+		//d := make(chan []byte)
+		stdout, errStdout = handeStream(os.Stdout, stdoutIn)
+		wg.Done()
+		select {
+		case <-cctx.Done():
+			if err := cmd.Process.Kill(); err != nil {
+				log.Fatal("failed to kill process: ", err)
+			}
+			log.Println("Process Killed")
+			return
+		}
+	}(ctx)
+	wg.Wait()
+
+	err = cmd.Wait()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatal("failed to capture stdout or stderr\n")
 	}
 
-	res := handleXML(out.String())
-	_, err = w.Write(res)
+	// fire a goroutine
+	go handleXML(stdout, res)
+
+	// wait for the result and pass the stream to browser
+	_, err = w.Write(<-res)
 	if err != nil {
 		w.Write([]byte("Something went wrong"))
-		return
 	}
 }
